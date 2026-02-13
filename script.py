bb099b0f1cff88827d602749725acd67773d8c58
Safal-gg <safal.shrestha355@gmail.com>
2026-02-13 10:38:47 +0545

    added more ascii characters and optimized the program by subrooting it

--- script.py
+++ script.py
@@ -1,4 +1,21 @@
 from PIL import Image
+
+#Resizing image into required dimensions.
+def resize(image):
+    image_width=image.width
+    image_height=image.height
+    new_width=100
+    new_height=((image_height/image_width)*new_width)*0.5
+    new_height=int(new_height)
+    resized_image=image.resize((new_width,new_height))
+    return resized_image
+    
+# Converting the resized image into grayscale.
+def grayscale(image):
+    grayscale_image=image.convert("L")
+    return grayscale_image
+
+# Main program responsible for loading image through image path and mainly doing ASCII operations.
 def main():
     image_path=input("Enter valid image path:")
 
@@ -7,20 +24,20 @@ def main():
     except:
         print(f"{image_path} is not a valid path!")
         return 0
-    print(image.size)
-    image_width=image.width
-    image_height=image.height
-    new_width=100
-    new_height=((image_height/image_width)*new_width)*0.5
-    resized_image=image.resize((new_width,int(new_height)))
-    print(resized_image.size)
-    grayscale_image=resized_image.convert("L")
-    print(grayscale_image)
-    ASCII_CHARS = ['.',',',':',';','+','*','?','%','S','#','@']
+    
+    #Resizing and grayscaling the image
+    resized_image=resize(image)
+    grayscale_image=grayscale(resized_image)
+
+    #Defining the ASCII characters
+    ASCII_CHARS = ['.',',','^',':',';','+','*','?','%','$','#','@']
     ascii_image = ""
-    for y in range(int(new_height)):
+
+    width, height = grayscale_image.size
+
+    for y in range(height):
         row = "" 
-        for x in range(new_width):
+        for x in range(width):
             pixel_value = grayscale_image.getpixel((x, y))
             index = pixel_value * (len(ASCII_CHARS) - 1) / 255
             index=int(index)
